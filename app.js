eb9de858048d95a5c7d1621badcdd9bdb8057e0c
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-18 17:59:08 +0530

    json parse

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.listen(3000, function () {
 app.get("/", function (req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=50fc4f26121627ba93652a34ac9f943d&units=metric"
     https.get(url, function (response) {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDesc = weatherData.weather[0].description;
+            console.log(weatherDesc);
+
+        })
     })
     res.send("hello");
 
