352b704635535dc6618d7b09946874a8df66cb25
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-18 16:53:00 +0530

    https get

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const https = require("https");
 app.listen(3000, function () {
     console.log("started");
 })
-app.get("/", function (req,res) {
+app.get("/", function (req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=50fc4f26121627ba93652a34ac9f943d&units=metric"
+    https.get(url, function (response) {
+        console.log(response);
+    })
     res.send("hello");
+
 })
